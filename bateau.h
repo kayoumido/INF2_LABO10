a7406e75b3c15acd4d63f8cc093c1f16e2bdd4b1
AloisChristen <alois.christen@heig-vd.ch>
2019-05-28 17:04:33 +0200

    bateau.h created

---
+++ bateau.h
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   navire.h
+ * Author: Alois
+ *
+ * Created on 28. mai 2019, 16:47
+ */
+
+#ifndef NAVIRE_H
+#define NAVIRE_H
+
+typedef enum {MOTEUR, VOILE, RAME} Type;
+typedef const char* No_plaque;
+
+typedef struct  {
+    unsigned short nb_moteurs;
+    double puissance;
+} Bateau_moteur;
+typedef struct  {
+    unsigned short nb_rames;
+} Bateau_rame;
+typedef struct  {
+    double surface; // mètre carrés
+} Bateau_voile;
+
+typedef union   {
+  Bateau_moteur moteur;  
+  Bateau_rame rame;  
+  Bateau_voile voile;  
+} Details;
+
+typedef struct  {
+  Type type;
+  No_plaque No;
+  double longeur; // mètres
+  Details details;
+} Bateau;
+
+Bateau saisirBateau();
+void afficherDetailsBateau(Bateau* b);
+
+#endif /* NAVIRE_H */
+
