6b435bf6504ac6359d9147753f018ff39a19239d
Doran Kayoumi <dorankayoumi@gmail.com>
2019-06-05 22:52:51 +0200

    Created tools file for helpful tools that could be used anywhere

---
+++ tools.h
@@ -0,0 +1,11 @@
+//
+// Created by deekay on 05/06/19.
+//
+
+#ifndef LABO10_TOOLS_H
+#define LABO10_TOOLS_H
+
+#define VIDER_BUFFER while(getchar() != '\n')
+#define NEW_LINE printf("\n")
+
+#endif //LABO10_TOOLS_H
