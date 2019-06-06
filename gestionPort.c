9dede7fd2f699a7974dd0f04dcc505fe290d2b2b
Doran Kayoumi <dorankayoumi@gmail.com>
2019-06-06 17:09:18 +0200

    Add comments

--- gestionPort.c
+++ gestionPort.c
@@ -10,68 +10,98 @@
 
 #include "tools.h"
 #include "gestionPort.h"
-#define CAPACITE_PORT 40
 
-#define VIDER_BUFFER while(getchar() != '\n')
-size_t premierEmplacementLibre(const Port port){
+
+/**
+ * @brief Parcours un port afin de trouver la première place disponible
+ * @param port a parcourirs
+ * @return la première place de libre
+ * @note s'il n'y a pas de place de libre, l'index retourné sera plus grande que la capacité du tabeau
+ */
+size_t premierEmplacementLibre(const Port port) {
     size_t i = 0;
-    while(i < CAPACITE_PORT && port[i] != NULL){
-        i++;
-    }
+
+    while (i < CAPACITE_PORT && port[i] != NULL) i++;
+
     return i;
 }
 
-bool placerBateau(const Bateau* bateau, Port port){
-    size_t i = premierEmplacementLibre(port);
-    if( i >= CAPACITE_PORT){
-        return false; // Pas d'emplacements libres
+/**
+ * @brief Parcours un port afin de trouver la position d'un bateau
+ * @param plaque du bateau cherché
+ * @param port
+ *
+ * @return la position du bateau s'il a été trouvé, sinon -1
+ */
+int trouverBateau(NoPlaque plaque, Port port) {
+    for (int i = 0; i < CAPACITE_PORT; ++i) {
+        if (port[i] && port[i]->no == plaque) {
+            return i;
+        }
     }
-    port[i] = (Bateau*) bateau;
+    return -1;
+}
+
+bool placerBateau(const Bateau *bateau, Port port) {
+    // Parcours du port pour vérifier si le bateau n'est pas déjà présent
+    // s'il est déjà présent, on ne fais rien
+    size_t place = trouverBateau(bateau->no, port);
+    if (place == -1)
+        return false;
+
+    // On vérifie s'il y a une place de livre de port
+    size_t i = premierEmplacementLibre(port);
+    if (i >= CAPACITE_PORT) {
+        return false;
+    }
+
+    // Tout est ok, alors le bateau peut être ajouter dans le port
+    port[i] = (Bateau *) bateau;
     return true;
 }
 
-size_t trouverEmplBateau(NoPlaque plaque, Port port){
-    size_t i = 0;
-    for(; i < CAPACITE_PORT; ++i){
-       if(port[i] && port[i]->no == plaque){
-           return i;
-       } 
-    }
-    return i;
-}
 
-void libererPlace(NoPlaque plaque, Port port){
-    size_t empl = trouverEmplBateau(plaque, port);
-    if(empl < CAPACITE_PORT){
-        free(port[empl]);
-        port[empl] = NULL;
+void libererPlace(NoPlaque plaque, Port port) {
+
+    // recherche du bateau
+    size_t place = trouverBateau(plaque, port);
+
+    if (place < CAPACITE_PORT) {
+        port[place] = NULL;
     }
 }
 
-void afficherDetails(NoPlaque plaque, Port port){
-    size_t empl = trouverEmplBateau(plaque, port);
-    if(empl < CAPACITE_PORT){
+void afficherDetails(NoPlaque plaque, Port port) {
+    size_t empl = trouverBateau(plaque, port);
+    if (empl < CAPACITE_PORT) {
+
+        printf("Details du bateau %s : \n", plaque);
         afficherDetailsBateau(port[empl]);
-    } 
+    }
 }
 
-void afficherPlace(size_t empl, Port port){
-    if(port[empl]){
-        afficherBateau(port[empl]);
+void afficherPlace(size_t place, Port port) {
+    printf("Bateau à la place %d : \n", place);
+
+    if (port[place]) {
+        afficherBateau(port[place]);
     } else {
         printf("-\n");
     }
+    NEW_LINE;
 }
 
-void parcours(Port port, void(*funct)(Bateau* bateau)){
-    for(size_t i = 0; i < CAPACITE_PORT; i++){
-        if(port[i]){
-            funct(port[i]);
-        }
+void parcours(Port port, void(*action)(Bateau *bateau)) {
+    for (size_t i = 0; i < CAPACITE_PORT; i++) {
+        if (port[i])
+            action(port[i]);
     }
 }
 
-void afficherPort(Port port){
+void afficherPort(Port port) {
+    printf("Affichage du port : \n");
+    printf("-------------------\n");
+
     parcours(port, afficherDetailsBateau);
 }
 
--- gestionPort.h
+++ gestionPort.h
@@ -15,16 +15,50 @@
 
 typedef Bateau* Port[CAPACITE_PORT];
 
+/**
+ * @brief Ajoute un bateau (pointeur) dans un port
+ *
+ * @param bateau Pointeur vers le bateau a ajouter au port
+ * @param port dans lequel on veut ajouter le bateau
+ * @return Vrai si le bateau a été ajouté, sinon faux
+ */
 bool placerBateau(const Bateau* bateau, Port port);
 
+/**
+ * @brief Libère une place dans le port
+ * @param plaque Le no de plaque du bateau
+ * @param port dans lequel on veut liberer une place
+ */
 void libererPlace(NoPlaque plaque, Port port);
 
+/**
+ * @brief Affiche le détail complet d'un bateau
+ *
+ * @param plaque du bateau afficher
+ * @param port
+ */
 void afficherDetails(NoPlaque plaque, Port port);
 
-void afficherPlace(size_t empl, Port port);
+/**
+ * @brief Affiche l'infos de base du bateau se trouvant à une place
+ *
+ * @param place que l'on veut afficher
+ * @param port
+ */
+void afficherPlace(size_t place, Port port);
 
-void parcours(Port port, void(*funct)(Bateau* bateau));
+/**
+ * @brief Permet un traitement à chacune des places du port.
+ *
+ * @param port a parcourir
+ * @param action le traitement a effectuer
+ */
+void parcours(Port port, void(*action)(Bateau* bateau));
 
+/**
+ * @brief Affiche un port
+ * @param port à afficher
+ */
 void afficherPort(Port port);
 
 #endif //LABO10_GESTIONPORT_H
